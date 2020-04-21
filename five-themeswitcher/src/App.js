91bb7e9f8c24c3649f0d5171d8bc8c288cd16c6f
prakash126 <prakashkumar845411@gmail.com>
2020-04-22 00:26:20 +0530

    Theme Toggler App

---
+++ five-themeswitcher/src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import ThemeContext from './Context/ThemeContext'
+import Header from './Components/Header'
+import HeroSection from './Components/HeroSection'
+
+const App  = ()=>{
+  const themeHook = useState("light")
+  return(
+    <ThemeContext.Provider value={themeHook}>
+        <div>
+          <Header />
+            <HeroSection>
+              
+            </HeroSection>
+          
+        </div>
+    </ThemeContext.Provider>
+  )
+}
+
+export default App;
+
+// import React, { useState } from "react";
+// import ThemeContext from "./Context/ThemeContext";
+// import Header from "./Components/Header";
+// import HeroSection from "./Components/HeroSection";
+
+// const App = () => {
+//   const themeHook = useState("light");
+//   return (
+//     <ThemeContext.Provider value={themeHook}>
+//       <div>
+//         <Header />
+//         <HeroSection />
+//       </div>
+//     </ThemeContext.Provider>
+//   );
+// };
+
+// export default App;
