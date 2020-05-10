5582c16f80f47e228daf295daa31ccdd92c4c874
prakash126 <prakashkumar845411@gmail.com>
2020-05-10 23:06:50 +0530

    githup api fetch user

---
+++ ten-github-app/src/Config/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyCHSig3TuidNsNTcUzzcwa90ymsn8QYmDs",
+    authDomain: "mygitapp-3099e.firebaseapp.com",
+    databaseURL: "https://mygitapp-3099e.firebaseio.com",
+    projectId: "mygitapp-3099e",
+    storageBucket: "mygitapp-3099e.appspot.com",
+    messagingSenderId: "264744429610",
+    appId: "1:264744429610:web:5fe2016e717054431ebb53",
+    measurementId: "G-L9XQ23QPJ1"
+  };
