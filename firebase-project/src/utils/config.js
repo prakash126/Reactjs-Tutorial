cc894668c06dfe09dbe0582bf8c2a683b258bdb9
prakash126 <prakashkumar845411@gmail.com>
2020-05-17 17:32:10 +0530

    firebase-realtime-database

---
+++ firebase-project/src/utils/config.js
@@ -0,0 +1,22 @@
+//TODO: DONE add firebase configuration and export it
+export const firebaseConfig = {
+  //TODO: ADD YOUR FIREBASE INFO HERE
+  
+    apiKey: "AIzaSyCHSig3TuidNsNTcUzzcwa90ymsn8QYmDs",
+    authDomain: "mygitapp-3099e.firebaseapp.com",
+    databaseURL: "https://mygitapp-3099e.firebaseio.com",
+    projectId: "mygitapp-3099e",
+    storageBucket: "mygitapp-3099e.appspot.com",
+    messagingSenderId: "264744429610",
+    appId: "1:264744429610:web:5fe2016e717054431ebb53",
+    measurementId: "G-L9XQ23QPJ1"
+  };
+
+
+//image configuration
+export const imageConfig = {
+  quality: 0.2,
+  maxWidth: 800,
+  maxHeight: 600,
+  autoRotate: true
+};
